345ab2a1b457db6a2594e5bb16390f17b59f5389
Your Name <your@email.com>
2017-05-16 16:49:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+test
+====
+
+A Symfony project created on May 15, 2017, 8:34 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\headers;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function showAction(Request $request)
+    {
+      $ip = $request->getClientIp();
+      $agent = $request->headers->get('User-Agent');
+return new Response('<html><body> <h1>Privet 4uvak</h1><br>
+<p><b>Your USER AGENT :</b> '.$agent.'</p>
+<b>Your IP ADRESS : </b> '.$ip .'
+</body></html>');
+ }
+
+}
